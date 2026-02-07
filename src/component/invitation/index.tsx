89a3ad085b7efc3864616f98b6990766349616fb
kimjy2630 <kimjy2630@naver.com>
2026-02-07 17:09:37 +0900

    date change

--- src/component/invitation/index.tsx
+++ src/component/invitation/index.tsx
@@ -25,17 +25,13 @@ export const Invitation = () => {
 
       <div className="break" />
 
-      <div className="content">싱그러운 여름 향기 가득한 날</div>
-      <div className="content">소중한 분들을 모시고</div>
-      <div className="content">사랑의 약속을 하려고 합니다.</div>
+      <div className="content">20대를 함께한 서로를 가장 닮은 두 사람이</div>
+      <div className="content">믿음의 가정 안에서 하나로 나아거려 합니다.</div>
       <div className="break" />
-      <div className="content">햇살이 뜨거울 땐 가려주고,</div>
-      <div className="content">비가 오면 우산이 되어주는</div>
-      <div className="content">부부가 되겠습니다.</div>
-      <div className="break" />
-      <div className="content">기쁜날 함께 하시어</div>
-      <div className="content">저희의 앞날을 축복해 주세요.</div>
-
+      <div className="content">저희의 인생의 전환점의 순간에 함께하시어</div>
+      <div className="content">기도와 함께 축복해주시고</div>
+      <div className="content">자리를 빛내주시면 감사하겠습니다.</div>
+      
       <div className="break" />
 
       <div className="name">
--- src/const.ts
+++ src/const.ts
@@ -9,15 +9,15 @@ dayjs.locale("ko")
 
 export { dayjs }
 
-export const WEDDING_DATE = dayjs.tz("2024-08-24 13:00", "Asia/Seoul")
+export const WEDDING_DATE = dayjs.tz("2026-04-25 12:30", "Asia/Seoul")
 export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.minute() === 0 ? "" : " m분"}`
 
 // 예식 당월 휴무일. 켈린더에 표시하기 위함.
 // 예: 예식일 8월 -> 8월 15일 광복절
 export const HOLIDAYS = [15]
 
-export const LOCATION = "서울대학교 연구공원 웨딩홀"
-export const LOCATION_ADDRESS = "서울시 관악구 관악로 1, 연구공원 본관 1층"
+export const LOCATION = "KAIST 노천극장"
+export const LOCATION_ADDRESS = "대전 유성구 대학로 291, 카이스트 본원 노천극장"
 
 // 카카오톡 공유 시 위치 정보로 사용할 주소.
 // LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
@@ -29,19 +29,19 @@ export const WEDDING_HALL_POSITION = [126.9594982, 37.4657134]
 
 // 네이버 지도의 웨딩홀 장소 ID
 // 네이버 지도 웹페이지에서 웨딩홀 검색 후 URL에서 확인 가능.
-// 예: https://map.naver.com/p/entry/place/13321741 -> 13321741
-export const NMAP_PLACE_ID = 13321741
+// 예: https://map.naver.com/p/entry/place/16097793 -> 16097793
+export const NMAP_PLACE_ID = 16097793
 
 // 카카오 지도의 웨딩홀 장소 ID
 // 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
-// 예: https://place.map.kakao.com/8634826 -> 8634826
-export const KMAP_PLACE_ID = 8634826
+// 예: https://place.map.kakao.com/17564974 -> 17564974
+export const KMAP_PLACE_ID = 17564974
 
-export const BRIDE_FULLNAME = "정지원"
-export const BRIDE_FIRSTNAME = "지원"
-export const BRIDE_TITLE = "장녀"
-export const BRIDE_FATHER = "정상원"
-export const BRIDE_MOTHER = "박윤정"
+export const BRIDE_FULLNAME = "황희선"
+export const BRIDE_FIRSTNAME = "희선"
+export const BRIDE_TITLE = "딸"
+export const BRIDE_FATHER = "황태근"
+export const BRIDE_MOTHER = "허영림"
 export const BRIDE_INFO = [
   {
     relation: "신부",
@@ -63,11 +63,11 @@ export const BRIDE_INFO = [
   },
 ]
 
-export const GROOM_FULLNAME = "남주호"
-export const GROOM_FIRSTNAME = "주호"
-export const GROOM_TITLE = "차남"
-export const GROOM_FATHER = "남현태"
-export const GROOM_MOTHER = "김현영"
+export const GROOM_FULLNAME = "김재윤"
+export const GROOM_FIRSTNAME = "재윤"
+export const GROOM_TITLE = "아들"
+export const GROOM_FATHER = "김경원"
+export const GROOM_MOTHER = "김미현"
 export const GROOM_INFO = [
   {
     relation: "신랑",
